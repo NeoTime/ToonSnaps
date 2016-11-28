cb7fef2c8d82b5b9e1070fbbda87bd05f890d955
Jefferey Rigler <jeff@riglerdigital.com>
2016-11-27 18:46:05 -0800

    Initial Commit

---
+++ ToonSnaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToonSnaps
+//
+//  Created by Jefferey Rigler on 11/27/16.
+//  Copyright © 2016 RiglerDigital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
